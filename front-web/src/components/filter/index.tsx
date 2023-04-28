45d4faedc7fe6187bc061e38862ac212534c1c3c
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2023-04-28 00:04:22 -0300

    Add Filter component

---
+++ front-web/src/components/filter/index.tsx
@@ -0,0 +1,36 @@
+import './styles.css';
+import 'flatpickr/dist/themes/material_green.css';
+import flatpickrLib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+import FlatPicker from 'react-flatpickr';
+
+flatpickrLib.localize(Portuguese);
+
+function Filter() {
+  const onChangeDate = (dates: Date[]) => {
+    console.log(dates);
+  };
+
+  return (
+    <div className="filter-container base-card">
+      <FlatPicker
+        options={{
+          mode: 'range',
+          dateFormat: 'd/m/Y',
+          showMonths: 2
+        }}
+        className="filter-input"
+        onChange={onChangeDate}
+        placeholder="Selecione um produto"
+      />
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="MALE"></option>
+        <option value="FEMALE"></option>
+        <option value="OTHER"></option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
